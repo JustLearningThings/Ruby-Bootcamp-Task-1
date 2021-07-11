12b86af605520ec8a309924a5ebecf1cb7acd085
Denny06 <denismacio@outlook.com>
2021-07-11 11:34:18 +0300

    adding tracks

--- main.rb
+++ main.rb
@@ -29,4 +29,14 @@ puts "Tokens check: #{auth.check_tokens(t1=ACCESS_TOKEN, t2=REFRESH_TOKEN)}"
 # Creating a playlist
 
 api = Spotify.new(username)
-api.create_playlist(auth)
+playlist_id = api.create_playlist("My new playlist", auth)
+
+# Adding some tracks to the created playlist
+
+snapshot_id = api.add_track_to_playlist(playlist_id, auth, 
+    api.get_track_id('numb', auth),
+    api.get_track_id('where is my mind', auth),
+    api.get_track_id('deutschland', auth)
+)
+
+# Place the first track at the end of the playlist
--- spotify.rb
+++ spotify.rb
@@ -3,17 +3,60 @@ require 'json'
 class Spotify
     def initialize(user_id)
         @USER_ID = user_id
-        @URL = "https://api.spotify.com/v1/users/#{@USER_ID}/playlists"
+        @CREATE_PLAYLIST_URL = "https://api.spotify.com/v1/users/#{@USER_ID}/playlists"
     end
 
-    def create_playlist(auth)
+    # returns the created playlist's id
+    def create_playlist(name, auth)
         # constructing the request body and adding headers
 
-        body = { name: 'My new playlist' }.to_json
+        body = { name: name }.to_json
 
         headers = { content_type: 'application/json' }
         auth.add_authorization_header(headers)
         
-        response = RestClient.post @URL, body, headers
+        response = RestClient.post @CREATE_PLAYLIST_URL, body, headers
+
+        #returning the playlist id
+
+        response = JSON.parse(response)
+        response['id']
+    end
+
+    # searches for a track by its name and returns its id
+    def get_track_id(name, auth)
+        # constructing the request body and adding headers
+
+        headers = { content_type: 'application/json' }
+        auth.add_authorization_header(headers)
+
+        url = "https://api.spotify.com/v1/search?q=#{name}&type=track&limit=1"
+
+        response = RestClient.get url, headers
+
+        # returning the track id
+
+        response = JSON.parse(response)
+        tracks = response['tracks']['items']
+
+        return tracks.empty? ? false : tracks[0]['id']
+    end
+
+    # returns the playlist's new snapshot id
+    def add_track_to_playlist(playlist_id, auth, *items)
+        # constructing the request body and adding headers
+
+        items.map! { |uri| uri.prepend("spotify:track:") }
+        body = { uris: items }.to_json
+
+        headers = { content_type: 'application/json' }
+        auth.add_authorization_header(headers)
+
+        url = "https://api.spotify.com/v1/playlists/#{playlist_id}/tracks"
+
+        response = RestClient.post url, body, headers
+        
+        response = JSON.parse(response)
+        response['snapshot_id']
     end
 end
